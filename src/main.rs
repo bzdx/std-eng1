31d86f56c7e7c690bc2049aa85f29c62283da50b
bzdx <huanghaobin@gamedachen.com>
2021-12-09 19:09:53 +0800

    fix(*): add google seo file

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,11 @@ async fn index() -> impl Responder {
   .body("")
 }
 
-// #[get("/baidu_verify_code-abGIytVVhs.html")]
-async fn baidu() -> Result<NamedFile> {
-  // println!("{}", 123123132);
+async fn baidu_seo() -> Result<NamedFile> {
   Ok(NamedFile::open("baidu_verify_code-CNu82SU367.html")?)
+}
+async fn google_seo() -> Result<NamedFile> {
+  Ok(NamedFile::open("google415cd2d0b77f2294.html")?)
 }
 
 #[actix_web::main]
@@ -22,7 +23,8 @@ async fn main() -> std::io::Result<()> {
   HttpServer::new(|| {
     App::new()
       .service(index)
-      .route("/baidu_verify_code-CNu82SU367.html", web::get().to(baidu)) // baidu SEO
+      .route("/baidu_verify_code-CNu82SU367.html", web::get().to(baidu_seo)) // baidu SEO
+      .route("/google415cd2d0b77f2294.html", web::get().to(google_seo)) // google SEO
       .service(Files::new("/", "dist").show_files_listing())
   })
   .bind(path)?
