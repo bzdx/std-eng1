28cd2f1c6980103d57b18a6753793f3a4590ca6a
bzdx <huanghaobin@gamedachen.com>
2021-12-13 12:30:27 +0800

    fix(*): fix baidu tongji domain

--- dist/baidu.js
+++ dist/baidu.js
@@ -1,1 +1,1 @@
-var _hmt=_hmt||[];!function(){var e=document.createElement("script");e.src="https://hm.baidu.com/hm.js?6575f7375ce103584485d339d6296d79";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)}();
+var _hmt=_hmt||[];!function(){var e=document.createElement("script");e.src="https://hm.baidu.com/hm.js?c056d44bddc725d7c04a15bd90e3d6d4";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)}();
